3aaf0202a75d92e768ac26a28734059ef27bcb0f
jagrutij300 <jagrutij300@gmail.com>
2023-11-02 12:41:20 +0530

    daily_js_practice

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+console.log('gfgfh');
+let state=""; //empty string
+
+let city="pune"; // string data type
+
+console.log('city 1',city);
+
+city="mumbai";
+
+console.log('city 2',city);
+
+
+var marks =90; //number
