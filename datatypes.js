9d75122d6e24b2598cbf51491d88570f72157426
jagrutij300 <jagrutij300@gmail.com>
2023-11-03 10:19:19 +0530

    daily_js_practice

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,6 @@
 console.log('gfgfh');
+
+// when we enter any word or commant in couma then these is string data type otherwise number or bollen //
 let state=""; //empty string
 
 let city="pune"; // string data type
@@ -11,3 +13,47 @@ console.log('city 2',city);
 
 
 var marks =90; //number
+console.log('marks', marks)
+
+
+var mobno =7083844236;
+console.log(mobno); //number
+
+var mobno ='7083844236';//string
+console.log(mobno);
+
+var isfavfruit = true; //boolen true or false
+console.log(isfavfruit);
+
+var isfavfruit = 'true'; //string true or false
+console.log(isfavfruit);
+
+
+//type of() .it gives type of variable
+console.log('data type of isfavfruit var',typeof(isfavfruit));
+
+const match= undefined;
+console.log(match);
+ 
+let abc;
+console.log(abc)
+
+var myname = null;
+console.log(myname);
+console.log(typeof(myname));
+
+let a = 999999999999999;
+console.log(a);
+
+let c = "9999999999999999";
+console.log(BigInt(c));
+
+let d = 9999999999999999n;
+console.log(d);
+
+
+
+
+//array
+let name = ""
+
