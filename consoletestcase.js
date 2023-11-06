1ac1647db9e9915e53cbfbf7452c1c6a248a4c21
jagrutij300 <jagrutij300@gmail.com>
2023-11-06 20:43:12 +0530

    console test case

--- consoletestcase.js
+++ consoletestcase.js
@@ -1,3 +1,16 @@
 console.log(); //inbuild function used to check value of variable
 
 let a = 10;
+console.log(a); //10
+console.log('a value>>',a);
+console.log('a value>>'+a); //+concatination operator 2.arithematic operator
+
+//arathematic operator
+let b = 20;
+console.log(a+b); //30
+
+let c ="30";
+console.log(b+c); //2030 one number one string 
+console.log(a+b+c); //3030
+console.log(c+b+a); //302010
+
