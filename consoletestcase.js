ffae829bc94238fe5c70721aacc37cb5d5d363ad
jagrutij300 <jagrutij300@gmail.com>
2023-11-07 20:14:39 +0530

    18.10.2023

--- consoletestcase.js
+++ consoletestcase.js
@@ -13,4 +13,24 @@ let c ="30";
 console.log(b+c); //2030 one number one string 
 console.log(a+b+c); //3030
 console.log(c+b+a); //302010
+console.log(b+a+c+a+b); //30201020
+
+let city ='mumbai';
+let d=40;
+console.log(d+city);
+console.log(city+d);
+console.log(d+city+b);
+console.log(d+city+b+a);
+console.log(a+d+city+b+a);
+
+console.log("a",a);
+console.log("a" +a);
+
+
+//onego value see all value
+console.log(a,b,c,d,city);
+console.log('a',a,'b',b,'c',c,'d',d,'city',city);
+
+
+
 
