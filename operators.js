701ce799175c2b14994a4d8e96e7a81832cee2f9
jagrutij300 <jagrutij300@gmail.com>
2023-11-07 20:47:25 +0530

    operatsor.js

---
+++ operators.js
@@ -0,0 +1,27 @@
+// =  assigment operator to assign value to the variable
+//== euality operators to compare value of two variable
+//=== euality operators it will compare value as well as data type
+
+let a = 40; //assigment operator
+
+let b = "30"
+
+console.log(a == b);//only value
+console.log(a === b); //it will strictly cheak value and data type
+
+let c = 20;
+var d = 30;
+
+console.log(c != d); //! not operator>>
+console.log(c == d);
+
+let isfav = true;
+console.log(!isfav); //false
+
+let e = 40;
+let f = 50;
+// > grater than
+console.log('>',a > f); //false
+console.log('<' a < f); //true
+
+
