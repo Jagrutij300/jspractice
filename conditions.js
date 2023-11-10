03786293485e21350f85249fe1b82e97f511ab36
jagrutij300 <jagrutij300@gmail.com>
2023-11-10 19:52:03 +0530

    conditional.js

--- conditions.js
+++ conditions.js
@@ -1,6 +1,6 @@
 //if condition,  syntex for if condition if(){code block}
 //if else, syntex for if else if(condition){code block}else{}
-//else if, 
+//else if, syntex if(){}else if{}else if{}...as so on else if else{}
 //switch
 // items greater than orequl to 50 
 let items = 40;
@@ -13,4 +13,19 @@ if(items >= 50){
 }
 else{console.log("limited stock available");}
 
+//else if multipal conditions
+if(items >=50){
+    console.log("stock avaliable");
+
+}
+else if(items >= 20)
+{
+    console.log("few items left");
+} 
+    
+else if(items >= 1){
+    console.log("hurry up few stock avaliable");
+} 
+else{
+    console.log("out of stock");}
 
