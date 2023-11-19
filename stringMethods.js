7a1615c28673b741ad3cd02f0814967bd3dad33d
jagrutij300 <jagrutij300@gmail.com>
2023-11-19 20:07:33 +0530

    stringMethods

--- stringMethods.js
+++ stringMethods.js
@@ -62,18 +62,63 @@ console.log(text.replace(/roha/ig,'mumbai'));//using  g in text all roha words r
  console.log(stringData);
 
 
- //toString() conerted into number to string
+ //toString() converted into number to string
  let num = 67679980;
  let data1 = num.toString();
  console.log(data1);
 
+ //parseIn coverted string into number
+ let num2 = '565656';
+ let m = parseInt(num2)
+
+
  //charAt()
  let text1 = "HELLO WORLD";
- let char = text1.charAt(0);
+ let char = text1.charAt(0);//H
  console.log(char);
- 
+ console.log(text1.charAt(20));//no indexavaliable so answer empty no answer or null or undefined
+
  //split()
  let date ='12/02/2002';
 let newDate = date.split('/') // it split out data and it will return array
 console.log(newDate);
 console.log(newDate[2]);
+
+let text4 = 'dkcjdslkfj kdjfir iojowufm kjhfkj';
+t4 = text4.split( ' ');
+console.log(t4);
+
+
+//string includes()
+let add = 'hsxbjs sjkxsh jagruti dnkedjfr';//include methode true if value include in data else will false
+console.log(add.includes('jagruti')); //sensitive means exact match jagruti word.
+
+//startswith
+//responce  show
+
+let StatusCode = '100256' 
+let a = StatusCode.startsWith('100');
+console.log(a);
+console.log(StatusCode.endsWith('256'));
+// indexof   first index
+let text5 = 'Hello World, Welcome to the universe welcome.';
+let result = text5.indexOf('Welcome');// inwelcome word then show index of W word alwys give first word meaning
+let result = text5.indexOf('toooooo');// answer is -1 bcz could not found 
+console.log(result);
+//lastindexof     find last index 
+text5.lastIndexOf('welcome')
+console.log(text5.lastIndexOf('welcome'));
+
+//search
+
+let text6 = 'Mr. Blue has a blue house';
+// let position = text6.search('blue');
+let position = text6.search(/blue/);//e can add regex pattern in search regex insensitive
+console.log(position);// diff betn search and indexof  method = we cannot pass regex pattern to index method
+
+//match()
+let text7 = ' The rain in SPAIN stays mainly in the plain';
+let g = text7.match('ain');
+console.log(g);
+console.log(text7.match(/ain/g));
+
