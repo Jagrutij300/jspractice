f812b38d94ace751066e92224f950293ec86e08b
jagrutij300 <jagrutij300@gmail.com>
2023-11-21 17:30:36 +0530

    array

--- arraymethods.js
+++ arraymethods.js
@@ -20,7 +20,7 @@ console.log(city);
 let colour = ['red','blue','black','yellow'];
 let shiftMethodReturnedData = colour.shift()
 console.log(colour);
-console.log(shiftMethodReturnedD);
+console.log(shiftMethodReturnedData);
 
 
 //unshift() // adding to first and show length
@@ -29,3 +29,21 @@ let colour2 = ['pink','red','blue','black','yellow'];
 let unshiftMethodReturnedData = colour2.unshift('black','orange',);
 console.log(colour2);
 console.log(unshiftMethodReturnedData);
+
+//join() in array different item joint we give by space ya other sign
+let v = ['pink','red','blue','black','yellow'];
+let v1 = v.join(' * ');
+console.log(v1);
+
+
+//concat()   means connected two or more string at that time
+cl2 = ['black','yellow'];
+cl3 = ['pink','red','blue'];
+let cl4 = cl2.concat(cl3,v);
+console.log(cl4);
+
+//delete operator
+const fruits = ['bannana','orange','apple','mango'];
+delete fruits[1];
+console.log(fruits);
+console.log(fruits.length);// length after deleting element same
