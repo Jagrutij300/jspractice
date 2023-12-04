323e6687d5a7be412cc8bb031c7c89feec22a452
jagrutij300 <jagrutij300@gmail.com>
2023-12-04 18:09:44 +0530

    databinding

--- myProject/src/app/home/home.component.ts
+++ myProject/src/app/home/home.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; //import section
 
-@Component({
+@Component({ //componenet directive : meta data of component
   selector: 'app-home',
   templateUrl:'./home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent { // componenet class : property ,constructor, life cycle hook, method, function etc.
+       //property section
+       //let/or/const city = "pune" var declaration in js
+       city : string = "pune"; //property
+       sName : any = 989898; // any means any thing number string or anything
+      Name : string = "jagruti";
+      Name2! : string; //only declare
+      Name3 : any; // any not need to ! declare
 
-  constructor(private router: Router){}
+
+
+  constructor(private router: Router){} // constructor section
+
+  //life cycle hook , Methods
   SignUpCompo(){
-    console.log("signcompo fun calling");
-    this.router.navigateByUrl("sign Up");
+    this.Name3 = 'OmSai';
+    console.log('Name3>>',this.Name3);
     
-
-  }
+    console.log("city>>",this.city);
+    console.log("signcompo fun calling");
+    this.test();
+    this.router.navigateByUrl("sign Up");
+     }
+     test(){
+      console.log("test calling...");
+      
+     }
 
 }
