4631bddadf386f8572dd5622fc53c9051fbc84d7
jagrutij300 <jagrutij300@gmail.com>
2023-12-04 14:22:42 +0530

    router

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     HomeComponent,
     LoginComponent,
+    SignUpComponent,
     
   ],
   imports: [
--- myProject/src/app/home/home.component.ts
+++ myProject/src/app/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl:'./home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private router: Router){}
+  SignUpCompo(){
+    console.log("signcompo fun calling");
+    this.router.navigateByUrl("sign Up");
+    
+
+  }
 
 }
