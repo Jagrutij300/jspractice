ffcb687fd11b0c27274fc7633bc7e71b8f89a56a
jagrutij300 <jagrutij300@gmail.com>
2023-12-06 08:14:22 +0530

    *ngif

--- myProject/src/app/directives/directives.component.ts
+++ myProject/src/app/directives/directives.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent {
+// *ngif directive: the ngif allows us to add/Remove Dom element
+city ="pune"
+  isShow = false; // null/undefined/false >>false 
 
+  //*ngFor
+  cars = ["i10","i20","swift","maruti"];
+  showDiv = true;
+  toggle(){
+    //this.showDiv="false";
+    this.showDiv = !this.showDiv; //!T >>F if>>t
+  }
 }
